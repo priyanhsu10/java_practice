00d32b5d9198b2da3399c6b0bc4808a4216b8a3c
priyanshu10 <priyanshuparate@gmail.com>
2022-05-21 10:54:18 +0530

    input using buffer reader

---
+++ src/main/java/basics/InputUsingBufferReader.java
@@ -0,0 +1,21 @@
+package basics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputUsingBufferReader {
+    public static void main(String[] args) {
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("enter the command:");
+        try {
+            String s= br.readLine();
+            System.out.println("command enter :"+ s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
