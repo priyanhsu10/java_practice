2ea9258d7f70fcdab0793761639b963250defec5
priyanshu10 <priyanshuparate@gmail.com>
2022-05-21 10:48:48 +0530

    basic

---
+++ src/main/java/basics/ScanTwoNumbers.java
@@ -0,0 +1,14 @@
+package basics;
+
+import java.util.Scanner;
+
+public class ScanTwoNumbers {
+        public static void main(String[] args) {
+            System.out.println("enter number");
+            Scanner sc= new Scanner(System.in);
+
+            int i= sc.nextInt();
+            System.out.println("number enter :"+i);
+
+        }
+}
