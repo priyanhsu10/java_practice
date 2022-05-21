3829ae345c0bc024d1f745966fe4a3593310b9c4
priyanshu10 <priyanshuparate@gmail.com>
2022-05-21 11:49:36 +0530

    swap numbers

--- src/main/java/basics/SwapTwoNumber.java
+++ src/main/java/basics/SwapTwoNumber.java
@@ -11,10 +11,25 @@ public class SwapTwoNumber {
         int a= sc.nextInt();
         System.out.println("Enter value ofb");
         int b= sc.nextInt();
-        a=a+b;
-        b=a-b;
-        a=a-b;
-        System.out.println("after swap a="+a +" and b="+b);
+//        addWithoutThird(a, b);
+        addUsingXOR(a,b);
 
     }
+
+    private static void addWithoutThird(int a, int b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("after swap a="+ a +" and b="+ b);
+    }
+    private  static  void addUsingXOR(int a, int b){
+
+        a=a^b;
+        System.out.println(a);
+        b=a^b;
+        System.out.println(b);
+        a=a^b;
+        System.out.println(a);
+    }
+
 }
