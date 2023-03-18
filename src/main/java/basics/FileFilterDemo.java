6d1bb1cf860ad3fcc6ceca4cc774f2b932348d3f
priyanshu10 <priyanshuparate@gmail.com>
2023-03-18 22:41:03 +0530

    hi

--- src/main/java/basics/FileFilterDemo.java
+++ src/main/java/basics/FileFilterDemo.java
@@ -2,20 +2,18 @@ package basics;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class FileFilterDemo {
     public static void main(String[] args) throws InterruptedException {
 
-        FileFilter filter= new FileFilter() {
+        FileFilter filter = new FileFilter() {
             @Override
             public boolean accept(File pathname) {
                 return pathname.getName().endsWith(".java");
             }
         };
 
-        File f= new File("/Users/priyanshuparate/projects/java/fundametals/java_practice/src/main/java/basics");
+        File f = new File("/Users/priyanshuparate/projects/java/fundametals/java_practice/src/main/java/basics");
 //        File[] files=f.listFiles(filter);
 //
 //      for(File fr:files) {
@@ -38,20 +36,40 @@ public class FileFilterDemo {
 //        t.start();
 //        t.join();
 //        System.out.println("hellow form thread "+ Thread.currentThread().getName());
+        //type of lamda expression is functional interface
 
 
-        Comparator<String> comp= new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return  Integer.compare(o2.length(),o1.length());
-            }
+//        Comparator<String> comp= new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return  Integer.compare(o2.length(),o1.length());
+//            }
+//        };
+//
+//        List<String> arb= Arrays.asList("***","**","****","*");
+//        Collections.sort(arb,comp);
+//        for(String s:arb){
+//            System.out.println(s);
+//        }
+        A.print();
+        A a = (int x) -> {
+            System.out.println("print x:" + x);
+            return x * x;
         };
 
-        List<String> arb= Arrays.asList("***","**","****","*");
-        Collections.sort(arb,comp);
-        for(String s:arb){
-            System.out.println(s);
+        a.printing();
+        a.x(10);
+    }
+
+    interface A {
+        int x(int x);
+
+        static void print() {
+            System.out.println("static method");
         }
 
+        default void printing() {
+            System.out.println("printing");
+        }
     }
 }
